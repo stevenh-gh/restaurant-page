183a443dbe9069a11b2dac82e6ea9a79f24f313f
steven <steven.h@live.com>
2021-03-30 18:27:18 -0700

    change textContent to specify its from home tab

--- src/tabHome.js
+++ src/tabHome.js
@@ -7,7 +7,7 @@ function tabHome() {
   let content = document.createElement('DIV')
   content.setAttribute('class', 'content')
   content.setAttribute('id', 'homeContent')
-  content.textContent = 'some content here...'
+  content.textContent = 'some content here...(home tab)'
   content.style.display = 'none'
 
 
